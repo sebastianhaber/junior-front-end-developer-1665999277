bb6e5bba4ea6fa6aee3f5f9ad0febddec9035147
Sebastian <hvber01@gmail.com>
2022-10-17 16:50:10 +0200

    Added tasks list

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { tasksList } from "../../mocked/tasks";
+import TaskListWrapper from "../organisms/tasksList/TaskListWrapper";
 import { StyledWrapper } from "./HomePage.styled";
 
 function HomePage() {
-  return <StyledWrapper></StyledWrapper>;
+  const [activeTask, setActiveTask] = useState(null);
+
+  useEffect(() => {
+    setActiveTask(tasksList[0]);
+  }, []);
+  return (
+    <StyledWrapper>
+      <TaskListWrapper setActiveTask={setActiveTask} />
+    </StyledWrapper>
+  );
 }
 
 export default HomePage;
