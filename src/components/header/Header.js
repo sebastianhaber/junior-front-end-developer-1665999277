c20233c6b3f0cad2c88e18c42f5cc61df8696337
Sebastian <hvber01@gmail.com>
2022-10-18 10:59:29 +0200

    Moved image with border to utils

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,18 +2,13 @@ import React from "react";
 import { StyledHeader } from "./Header.styled";
 import LogoSVG from "../../assets/logo.svg";
 import ProfileImage from "../../assets/profile_image.png";
+import Image from "../../utils/Image";
 
 function Header() {
   return (
     <StyledHeader>
       <img src={LogoSVG} alt="nerds.family" />
-      <div className="profileImage__wrapper">
-        <img
-          src={ProfileImage}
-          alt="Sebastian Haber"
-          className="profileImage__wrapper__image"
-        />
-      </div>
+      <Image url={ProfileImage} alt="Sebastian Haber" />
     </StyledHeader>
   );
 }
--- src/components/header/Header.styled.js
+++ src/components/header/Header.styled.js
@@ -8,18 +8,4 @@ export const StyledHeader = styled.nav`
   img {
     display: block;
   }
-  .profileImage__wrapper {
-    border: 1px solid #6b778c;
-    border-radius: 50%;
-    width: 46px;
-    height: 46px;
-    display: grid;
-    place-content: center;
-    cursor: pointer;
-    &__image {
-      width: 42px;
-      height: 42px;
-      border-radius: 50%;
-    }
-  }
 `;
