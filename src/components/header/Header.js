419e8ef9b5b6c783079bd3b027e6c5b4d01a81d1
Sebastian <hvber01@gmail.com>
2022-10-19 13:26:06 +0200

    Changed profile image

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,15 @@
-import React from "react";
 import { StyledHeader } from "./Header.styled";
 import LogoSVG from "../../assets/logo.svg";
-import ProfileImage from "../../assets/profile_image.png";
 import Image from "../../utils/Image";
 
 function Header() {
   return (
     <StyledHeader>
-      <img src={LogoSVG} alt="nerds.family" />
-      <Image url={ProfileImage} alt="Sebastian Haber" />
+      <img src={LogoSVG} alt="nerds.family" id="logo" />
+      <Image
+        url="https://randus.org/photos/w/00181c3c3c3c1c1e.jpg"
+        alt="Sebastian Haber"
+      />
     </StyledHeader>
   );
 }
